93b8f5995113cf605d67c613cd6cfed048b7998d
Jefferson Licet <jeffersonlicet@gmail.com>
2020-10-28 11:44:25 -0300

    Rebase to new version and maximun alternative

--- tp.hs
+++ tp.hs
@@ -1,15 +1,15 @@
-------------------
--- Ejercicio 1
--- Calcula el Modelo Exponencial Discreto
-------------------
+{-
+  Ejercicio 1
+  Calcula el Modelo Exponencial Discreto
+-}
 med :: Float -> Float -> Int -> Float
 med i0 b n | n == 0 = i0
            | otherwise = (med i0 b (n-1)) * (1 + b)
 
-------------------
--- Ejercicio 2
--- Calcula el Modelo Logistico Discreto
-------------------
+{-
+  Ejercicio 2
+  Calcula el Modelo Logistico Discreto
+-}
 mld :: Float -> Float -> Float -> Int -> Float
 mld p i0 b n
   | n == 0 = i0
@@ -17,45 +17,45 @@ mld p i0 b n
   where infectados = mld p i0 b (n - 1)
         sanosAnteriores = (p - infectados) / p
 
-------------------
--- Ejercicio 3
--- Calcula el Modelo SIR Discreto
-------------------
+{-
+  Ejercicio 3
+  Calcula el Modelo SIR Discreto
+-}
 sir :: (Float, Float, Float) -> Float -> Float -> Int -> (Float, Float, Float)
 sir (s0, i0, r0) b g n = (sanos, infectados, recuperados)
-  where sanos = calcularSanos params
+  where params = ((s0, i0, r0), b, g, n)
+        sanos = calcularSanos params
         infectados = calcularInfectados params
         recuperados = calcularRecuperados params
-        params = ((s0, i0, r0), b, g, n)
 
-------------------
--- Ejercicio 4
--- Calcula el Modelo SIR y retorna el maximo de infectados en n dias
-------------------
+{-
+  Ejercicio 4
+  Calcula el Modelo SIR y retorna el maximo de infectados en n dias
+-}
 maxsir :: (Float, Float, Float) -> Float -> Float -> Int -> Float
 maxsir (s0, i0, r0) b g n = maxSir' (s0, i0, r0) b g n (takeSecond sirN)
   where sirN = sir (s0, i0, r0) b g n
 
-------------------
--- Funciones auxiliares
-------------------
+{-
+  Funciones auxiliares
+-}
 
-------------------
+{-
 -- maxSir'
 -- Hace recursion desde n hasta 0 calculando sir(n) y conservando el máximo
-------------------
+-}
 maxSir' :: (Float, Float, Float) -> Float -> Float -> Int -> Float -> Float
 maxSir' (s0, i0, r0) b g n max'
   | n == 0 = max'
   | otherwise = maxSir' (s0, i0, r0) b g (n-1) nuevoMax
   where sirAnterior = sir (s0, i0, r0) b g (n-1)
-        nuevoMax = maximum[takeSecond sirAnterior, max']
+        nuevoMax = maximo (takeSecond sirAnterior) max'
 
-------------------
+{-
 -- calcularInfectados
 -- Implementación de I(t) para el modelo SIR discreto
 -- Hace recursion sobre n hasta 0
-------------------
+-}
 calcularInfectados :: ((Float, Float, Float), Float, Float, Int) -> Float
 calcularInfectados ((s0, i0, r0), b, g, n)
   | n == 0 = i0
@@ -64,11 +64,11 @@ calcularInfectados ((s0, i0, r0), b, g, n)
         sanos = calcularSanos params
         infectados = calcularInfectados params
 
-------------------
+{-
 -- calcularSanos
 -- Implementación de S(t) para el modelo SIR discreto
 -- Hace recursion sobre n hasta 0
-------------------
+-}
 calcularSanos :: ((Float, Float, Float), Float, Float, Int) -> Float
 calcularSanos ((s0, i0, r0), b, g, n)
   | n == 0 = s0
@@ -77,11 +77,11 @@ calcularSanos ((s0, i0, r0), b, g, n)
         sanosPrev = calcularSanos params
         infectados = calcularInfectados params
 
-------------------
+{-
 -- calcularRecuperados
 -- Implementación de R(t) para el modelo SIR discreto
 -- Hace recursion sobre n hasta 0
-------------------
+-}
 calcularRecuperados :: ((Float, Float, Float), Float, Float, Int) -> Float
 calcularRecuperados ((s0, i0, r0), b, g, n)
   | n == 0 = r0
@@ -90,10 +90,19 @@ calcularRecuperados ((s0, i0, r0), b, g, n)
         infectados = calcularInfectados params
         recuperados = calcularRecuperados params
 
-------------------
+{-
 -- takeSecond
 -- Funcion de ayuda para deconstruir el vector de SIR
 -- Obtiene la segunda entrada de un vector de tres elementos
-------------------
+-}
 takeSecond :: (Float, Float, Float) -> Float
 takeSecond (_, p, _) = p
+
+
+{-
+-- maximo
+-- Funcion de ayuda para obtener el maximo entre dos Floats
+-}
+maximo :: Float -> Float -> Float
+maximo a b | a >= b = a
+           | otherwise = b
